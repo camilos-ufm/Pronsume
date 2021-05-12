3f52a62386dddde75fa7656be6971549ad3b0fa2
danisnowman <danielernesto@ufm.edu>
2021-05-11 21:13:33 -0600

    Adds failed monitor attempt

--- Buffer.cs
+++ Buffer.cs
@@ -21,87 +21,106 @@ namespace ProyectoFinal
 
             Action producer = () =>
             {
-                lock (syncRoot)
+                // lock (syncRoot)
+                // {
+
+                Monitor.Enter(syncRoot);
+                Monitor.Pulse(syncRoot);
+                while (true)
                 {
-
-                    while (true)
+                    try
                     {
-                        Monitor.Pulse(syncRoot);
-                        try
+                        var nonProducedPersons = from myobject in listOfPersons
+                                                 where myobject.is_produced == false
+                                                 select myobject;
+                        foreach (var nonProducedPerson in nonProducedPersons)
                         {
-                            var nonProducedPersons = from myobject in listOfPersons
-                                                     where myobject.is_produced == false
-                                                     select myobject;
-                            foreach (var nonProducedPerson in nonProducedPersons)
+                            Thread.Sleep(500);
+                            if (producedCount == bufferSize)
                             {
-                                Thread.Sleep(500);
-                                if (producedCount == bufferSize)
-                                {
-                                    Console.WriteLine($"*****Buffer has filled up {productsBuffer.Count} == Cnt: {producedCount}*****");
-                                    Monitor.Wait(syncRoot);
-                                    Monitor.Exit(syncRoot);
-                                    // productsBuffer.CompleteAdding();
+                                Console.WriteLine($"*****Buffer has filled up {productsBuffer.Count} == Cnt: {producedCount}*****");
+                                Monitor.Wait(syncRoot);
+                                Monitor.Pulse(syncRoot);
+                                // Monitor.Exit(syncRoot); 
+                                // productsBuffer.CompleteAdding();
 
-                                }
-                                else
-                                {
-                                    Monitor.Enter(syncRoot);
-                                    Monitor.Pulse(syncRoot);
-                                    productsBuffer.Add(nonProducedPerson);
-                                    Console.WriteLine($"Producing {producedCount} {nonProducedPerson.name} w/ {Thread.CurrentThread.Name}");
-                                    nonProducedPerson.is_produced = true;
-                                    producedCount++;
-                                }
+                            }
+                            else
+                            {
+                                Monitor.Pulse(syncRoot);
+                                Monitor.Enter(syncRoot);
+                                nonProducedPerson.is_produced = true;
+                                Console.WriteLine($"Producing {producedCount} {nonProducedPerson.name} {nonProducedPerson.is_produced} w/ {Thread.CurrentThread.Name}");
+                                productsBuffer.Add(nonProducedPerson);
+                                producedCount++;
                             }
                         }
-                        catch (System.Exception e)
-                        {
-
-                            Console.WriteLine(e.Message);
-                        }
-                        Monitor.Pulse(syncRoot);
-                        Monitor.Wait(syncRoot);
                     }
+                    catch (System.Exception e)
+                    {
+
+                        Console.WriteLine(e.Message);
+                    }
+                    finally
+                    {
+
+                        Monitor.Exit(syncRoot);
+                        Monitor.Pulse(syncRoot);
+                    }
+                    // Monitor.Pulse(syncRoot);
+                    // Monitor.Wait(syncRoot);
                 }
+                // }
             };
 
             Action consumer = () =>
             {
-                lock (syncRoot)
-                    while (true)
+                // lock (syncRoot)
+                // Monitor.Enter(syncRoot);
+                Thread.Sleep(500);
+                while (true)
+                {
+                    Console.WriteLine($"Consuming w/ thread: {Thread.CurrentThread.Name}");
+                    Console.WriteLine($"producedCount: {producedCount}");
+                    // if (producedCount == 5)
+                    // {
+                    foreach (Person personConsumed in productsBuffer.GetConsumingEnumerable())
                     {
+                        // Monitor.Enter(syncRoot);
+                        // Monitor.Pulse(syncRoot);
                         // Thread.Sleep(500);
-                        Console.WriteLine($"Consuming w/ thread: {Thread.CurrentThread.Name}");
-                        Console.WriteLine($"producedCount: {producedCount}");
-                        if (producedCount == 5)
-                        {
-                            foreach (Person personConsumed in productsBuffer.GetConsumingEnumerable())
-                            {
-                                Monitor.Enter(syncRoot);
-                                Monitor.Pulse(syncRoot);
-                                Thread.Sleep(500);
-                                var sql_c = new SqlConnector("localhost", "dbuser", "password", "db");
-                                Console.WriteLine($"Person to consume: {personConsumed.name} Cnt: {productsBuffer.Count}");
-                                sql_c.insertIntoTable(personConsumed, Thread.CurrentThread.Name);
-                                producedCount--;
-                                Console.WriteLine($"Produced Count 0 == {producedCount}");
-                                Monitor.Exit(syncRoot);
-                                Monitor.Pulse(syncRoot);
-                            }
-                            Console.WriteLine("Trying to takeout every element of collection.");
-                        }
-
-                        Monitor.Wait(syncRoot);
+                        var sql_c = new SqlConnector("localhost", "dbuser", "password", "db");
+                        Console.WriteLine($"Person to consume: {personConsumed.name} Cnt: {productsBuffer.Count}");
+                        sql_c.insertIntoTable(personConsumed, Thread.CurrentThread.Name);
+                        producedCount--;
+                        Console.WriteLine($"Produced Count 0 == {producedCount}");
+                        Monitor.Exit(syncRoot);
+                        Monitor.Pulse(syncRoot);
                     }
+                    Console.WriteLine("Trying to takeout every element of collection.");
+                    // }
+                    if (producedCount == 0)
+                    {
+                        Console.WriteLine("ProduceCount 000000000");
+                        Monitor.Exit(syncRoot);
+                        Monitor.Pulse(syncRoot);
+                        // Thread.Sleep(500);
+                        // Monitor.Wait(syncRoot);
+                    }
+
+                    // Monitor.Wait(syncRoot);
+                }
             };
 
             var handle = new EventWaitHandle(false, EventResetMode.ManualReset);
+            var handle2 = new EventWaitHandle(false, EventResetMode.ManualReset);
             for (int i = 0; i < numOfThreads; i++)
             {
                 Task.Factory.StartNew(() =>
                 {
                     Thread.CurrentThread.Name = $"P{i}";
                     producer();
+                    handle.Set();
                 });
             }
             for (int i = 0; i < consumersSize; i++)
@@ -110,8 +129,11 @@ namespace ProyectoFinal
                 {
                     Thread.CurrentThread.Name = $"C{i}";
                     consumer();
+                    handle2.Set();
                 });
             }
+            handle.WaitOne();
+            handle2.WaitOne();
 
             Console.ReadLine();
         }
